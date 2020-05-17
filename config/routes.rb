fed9711723f7ff2c692922f5be463a37f45477c6
Victor Yeo <you@example.com>
2020-05-17 16:57:45 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'videos#index'
+  resources :videos
+  get '/videos/:id/like' => 'videos#like'
+  get '/videos/:id/dislike' => 'videos#dislike'
+  devise_for :users
+  resources :users
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
